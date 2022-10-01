bbc4b9a3efafec5cf143a14af116f7af268e54b0
Lucas Coelho Santos <lucascoelho375@gmail.com>
2022-09-30 23:43:58 -0400

    fix: formatacao dos arquivos .vue com a extensao Prettier

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,16 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <v-toolbar v-if="!usuarioLogado" color="primary" dark >
-    <div >
+  <v-toolbar v-if="!usuarioLogado" color="primary" dark>
+    <div>
       <v-btn class="d-flex align-center" text to="/">
-      <v-img
-        alt="Vuetify Logo"
-        class="shrink mr-2"
-        contain
-        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-        transition="scale-transition"
-        width="40"/>
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
 
         <v-img
           alt="Vuetify Name"
@@ -17,7 +18,8 @@
           contain
           min-width="100"
           src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"/>
+          width="100"
+        />
       </v-btn>
     </div>
 
@@ -32,14 +34,14 @@
       </v-btn>
     </v-toolbar-items>
   </v-toolbar>
-  
+
   <v-toolbar v-else>
     <v-toolbar-title class="headline text-uppercase mr-4">
       <span>{{ nome }}</span>
     </v-toolbar-title>
-    
+
     <v-spacer></v-spacer>
-    
+
     <v-toolbar-items>
       <v-btn @click="userLog" text to="/">Sair</v-btn>
     </v-toolbar-items>
@@ -50,18 +52,16 @@
 export default {
   data() {
     return {
-      nome: 'Yasmin Lied',
-      usuarioLogado: false
-    }
+      nome: "Yasmin Lied",
+      usuarioLogado: false,
+    };
   },
   methods: {
     userLog() {
-      this.usuarioLogado = !this.usuarioLogado
-    }
+      this.usuarioLogado = !this.usuarioLogado;
+    },
   },
-}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,14 +11,17 @@
       </v-col>
 
       <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">Bem vindo ao Sistema de Controle de Finanças</h1>
+        <h1 class="display-2 font-weight-bold mb-3">
+          Bem vindo ao Sistema de Controle de Finanças
+        </h1>
 
-        <p class="subheading font-weight-regular"><a href="/entrar">Entre</a> ou faça seu <a href="/registrar">registro</a> para começar!</p>
+        <p class="subheading font-weight-regular">
+          <a href="/entrar">Entre</a> ou faça seu
+          <a href="/registrar">registro</a> para começar!
+        </p>
       </v-col>
 
-      <v-col
-        class="mb-5"
-        cols="12">
+      <v-col class="mb-5" cols="12">
         <h2 class="headline font-weight-bold mb-3">Informações</h2>
 
         <v-row justify="center">
@@ -27,7 +30,8 @@
             :key="i"
             :href="next.href"
             class="subheading mx-3"
-            target="_blank">
+            target="_blank"
+          >
             {{ next.text }}
           </a>
         </v-row>
@@ -37,20 +41,20 @@
 </template>
 
 <script>
-  export default {
-    name: 'HomePage',
-    data: () => ({
-      importantLinks: [
-        {
-          text: 'Documentação',
-          href: 'https://github.com/LucasCoelhoSantos2/sistema-de-controle-de-financas',
-        },
-      ],
-    }),
-    methods: {
-      userLog() {
-        this.usuarioLogado = true
-      }
-    }
-  }
+export default {
+  name: "HomePage",
+  data: () => ({
+    importantLinks: [
+      {
+        text: "Documentação",
+        href: "https://github.com/LucasCoelhoSantos2/sistema-de-controle-de-financas",
+      },
+    ],
+  }),
+  methods: {
+    userLog() {
+      this.usuarioLogado = true;
+    },
+  },
+};
 </script>
